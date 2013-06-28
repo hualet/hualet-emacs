7e5314e6d3c81942c096ab1343de4d1c358817ed
Hualet Wang <mr.asianwang@gmail.com>
2013-06-28 11:25:42 +0800

    copy init-dired.el from deepin-emacs, omit files match regex like '.#'

--- Site-Lisp/Configure/Init/init-dired.el
+++ Site-Lisp/Configure/Init/init-dired.el
@@ -75,16 +75,22 @@
 
 
 ;;; Code:
-(require 'dired-x)
 
 (setq dired-recursive-copies t)                        ;可以递归的进行拷贝
 (setq dired-recursive-deletes t)                       ;可以递归的删除目录
+(setq dired-recursive-deletes 'always)                 ;删除东西时不提示
 (setq dired-recursive-copies 'always)                  ;拷贝东西时不提示
+;(toggle-dired-find-file-reuse-dir 1)                   ;使用单一模式浏览Dired
 (setq dired-details-hidden-string "[ ... ] ")          ;设置隐藏dired里面详细信息的字符串
 (setq dired-listing-switches "-aluh")                  ;传给 ls 的参数
 (setq directory-free-space-args "-Pkh")                ;目录空间选项
 (setq dired-omit-size-limit nil)                       ;dired忽略的上限
 (setq dired-dwim-target t)                             ;Dired试着猜处默认的目标目录
+(setq my-dired-omit-status t)                          ;设置默认忽略文件
+(setq my-dired-omit-regexp "^\\.?#\\|^\\..*")          ;设置忽略文件的匹配正则表达式
+(setq my-dired-omit-extensions '(".cache"))            ;设置忽略文件的扩展名列表
+(add-hook 'dired-after-readin-hook 'dired-sort-method) ;先显示目录, 然后显示文件
+(add-hook 'dired-mode-hook 'dired-omit-method)         ;隐藏文件的方法
 (setq dired-guess-shell-alist-user                     ;设置文件默认打开的模式
       '(
         ;; 图书
@@ -93,13 +99,19 @@
                           (replace-regexp-in-string ;替换空格为%20
                            " " "%20" (w3m-expand-file-name-as-url (dired-get-filename))) ;用URL的模式解析文件名
                           " -q"))
+        (list "\\.pdf$" "wine /data/Backup/WindowsTools/FoxitReader/FoxitReader.exe")
+        (list "\\.pdg$" "wine /data/Backup/WindowsTools/MiniPDG/pdgreader.exe")
         ;; 多媒体
-        ;; (list (format "\\(%s\\)$" (emms-player-get emms-player-mplayer 'regex)) "mplayer")
+        (list (format "\\(%s\\)$" (emms-player-get emms-player-mplayer 'regex)) "mplayer")
         (list "\\.\\(jpe?g\\|png\\)$" "eog" " -q")
         ;; 网页
-        (list "\\.html?$" "chrome")
+        (list "\\.html?$" "firefox")
         ;; 压缩包
-))
+        (list "\\.rar$" "unrar e -ad")
+        (list "\\.tar.bz2$" "tar jxvf")
+        (list "\\.gz$" "gzip -d")
+        ;; 其他
+        (list "\\.exe$" "wine")))
 
 (provide 'init-dired)
 
